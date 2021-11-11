54591b8ea0e35c48d7dd7253ca7a59fdc63d1db2
hx-w <herixth@163.com>
2021-11-11 16:05:47 +0800

    abstract __merge method

--- py/scripts.py
+++ py/scripts.py
@@ -10,6 +10,60 @@ from config import gconfig
 align = openpyxl.styles.Alignment(
     horizontal='left', vertical='center', wrap_text=False)
 
+async def __exec(source_dir: str, file_list: list, dist_file: str, template_file: str, newcol_pattern: re.Pattern = None):
+    shutil.copyfile(template_file, dist_file)
+
+    dist_book = openpyxl.load_workbook(dist_file)
+    dist_sheet = dist_book['工作任务项']
+    dist_sheet.delete_rows(2, dist_sheet.max_row + 1)
+    col_name, col_date = 1, 0
+    for idx in range(1, 1 + dist_sheet.max_column):
+        if dist_sheet.cell(1, idx).value == '日期（年/月/日）':
+            col_date = idx - 1
+            break
+    total_order = 1
+    for eachfile in file_list:
+        newcol = re.findall(newcol_pattern, eachfile)[0] if newcol_pattern else None
+        full_path = os.path.join(source_dir, eachfile)
+        source_sheet = openpyxl.load_workbook(
+            full_path, read_only=True)['工作任务项']
+        # print(source_sheet[1])
+        for row_idx in range(2, source_sheet.max_row + 1):
+            row_element = []
+            for col_idx in range(1, source_sheet.max_column + 1):
+                row_element.append(source_sheet.cell(
+                    row=row_idx, column=col_idx).value)
+            if None in row_element:
+                break
+            if newcol:
+                row_element.insert(col_name, newcol)
+            if row_element[col_date] and not isinstance(row_element[col_date], str):
+                row_element[col_date] = datetime.datetime.strftime(
+                    row_element[col_date], "%Y/%m/%d")
+            row_element[0] = total_order
+            dist_sheet.append(row_element)
+            total_order += 1
+
+    # align to left
+    for row in dist_sheet.iter_rows(min_row=2):
+        for col in row:
+            col.alignment = align
+
+    # update pivot table
+    for sheet_idx in range(len(dist_book.sheetnames)):
+        if dist_book.sheetnames[sheet_idx] == '工作任务项':
+            continue
+        try:
+            pivot_sheet = dist_book[dist_book.sheetnames[sheet_idx]]
+            pivot = pivot_sheet._pivots[0]  # 任何一个都可以共享同一个缓存
+            boundary = f'A1:{chr(ord("A") + dist_sheet.max_column - 1)}{dist_sheet.max_row}'
+            pivot.cache.cacheSource.worksheetSource.ref = boundary
+            pivot.cache.refreshOnLoad = True  # 刷新加载
+        except: pass
+    dist_book.save(dist_file)
+
+
+
 async def info_week_list(reverse: bool, single_week: bool) -> list:
     weeklist = list(filter(
         lambda x: re.match(gconfig.week_pattern, x),
@@ -42,6 +96,8 @@ async def swmg_group_list(week: str) -> list:
         file_list
     ))
     return file_list
+
+
 
 
 async def swsg_name_list(source_dir: str, week: str) -> list:
@@ -80,61 +136,12 @@ async def swsg_exec_merge(group_name: str, week: str, filelist: list, force: boo
     if os.path.exists(dist_path) and not force:
         return False, dist_path
     # cp template
-    shutil.copyfile(gconfig.template_group, dist_path)
+    try:
+        await __exec(source_dir, filelist, dist_path, gconfig.template_group, pattern)
+        return True, dist_path
+    except Exception as ept:
+        return False, f'{ept}'
 
-    dist_book = openpyxl.load_workbook(dist_path)
-    dist_sheet = dist_book['工作任务项']
-    dist_sheet.delete_rows(2, dist_sheet.max_row + 1)
-    col_name, col_date = 0, 0
-    for idx in range(1, 1 + dist_sheet.max_column):
-        if dist_sheet.cell(1, idx).value == '姓名':
-            col_name = idx - 1
-        elif dist_sheet.cell(1, idx).value == '日期（年/月/日）':
-            col_date = idx - 1
-    total_order = 1
-
-    for eachfile in filelist:
-        name = re.findall(pattern, eachfile)[0]
-        full_path = os.path.join(source_dir, eachfile)
-        source_sheet = openpyxl.load_workbook(
-            full_path, read_only=True)['工作任务项']
-        # print(source_sheet[1])
-        for row_idx in range(2, source_sheet.max_row + 1):
-            row_element = []
-            for col_idx in range(1, source_sheet.max_column + 1):
-                row_element.append(source_sheet.cell(
-                    row=row_idx, column=col_idx).value)
-            if None in row_element:
-                break
-            row_element.insert(col_name, name)
-            if row_element[col_date] and not isinstance(row_element[col_date], str):
-                row_element[col_date] = datetime.datetime.strftime(
-                    row_element[col_date], "%Y/%m/%d")
-            row_element[0] = total_order
-            dist_sheet.append(row_element)
-            total_order += 1
-
-    # align to left
-    for row in dist_sheet.iter_rows(min_row=2):
-        for col in row:
-            col.alignment = align
-
-    # update pivot table
-    for sheet_idx in range(len(dist_book.sheetnames)):
-        if dist_book.sheetnames[sheet_idx] == '工作任务项':
-            continue
-        try:
-            pivot_sheet = dist_book[dist_book.sheetnames[sheet_idx]]
-            pivot = pivot_sheet._pivots[0]  # 任何一个都可以共享同一个缓存
-            boundary = f'A1:{chr(ord("A") + dist_sheet.max_column - 1)}{dist_sheet.max_row}'
-            pivot.cache.cacheSource.worksheetSource.ref = boundary
-            pivot.cache.refreshOnLoad = True  # 刷新加载
-        except:
-            pass
-
-    dist_book.save(dist_path)
-
-    return True, dist_path
 
 
 import socket
